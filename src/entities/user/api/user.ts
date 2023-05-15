7cdd1d38d9c6168466a3c5af9d360d3e2a72ca44
Ripo2023 <sergeystaruskin@gmail.com>
2023-05-15 17:42:05 +0300

    improve: remove unnecessary console

--- src/entities/user/api/user.ts
+++ src/entities/user/api/user.ts
@@ -3,7 +3,6 @@ import { instance } from "../../../shared/api"
 export const getAllOrders = async () => {
     const response = await instance.get("getAllOrders");
 
-    console.log(response.data)
     
      return response.data;
 }
